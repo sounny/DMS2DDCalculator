0325773a709f6c25ddff2d9b4c414f6dd48a1e87
sounny <sounny@gmail.com>
2025-06-18 19:14:52 -0400

    Add DMS to DD converter with Leaflet map and quiz

---
+++ js/map.js
@@ -0,0 +1,27 @@
+let map, marker;
+
+export function initMap() {
+  map = L.map('map').setView([0, 0], 2);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map);
+  marker = L.marker([0, 0]).addTo(map);
+}
+
+export function updateMarker(lat, lon, text = '') {
+  if (!map) return;
+  marker.setLatLng([lat, lon]).bindPopup(text).openPopup();
+  map.setView([lat, lon], 8);
+}
+
+export function useMyLocation() {
+  if (!navigator.geolocation) {
+    alert('Geolocation not supported');
+    return;
+  }
+  navigator.geolocation.getCurrentPosition(pos => {
+    const { latitude, longitude } = pos.coords;
+    updateMarker(latitude, longitude, 'Your location');
+  });
+}
